5e7ecda82c5db7acce0a87690edcf8e04c93827a
Michael Brooks <mikeyabrooks@gmail.com>
2018-02-17 12:18:08 -0800

    refactor numbered card values to a range

--- deck.rb
+++ deck.rb
@@ -3,7 +3,7 @@ require './card'
 class Deck
 
   def initialize
-    @values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+    @values = ['Ace', *(2..10), 'Jack', 'Queen', 'King']
     @suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
     @cards = []
 
