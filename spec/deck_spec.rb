4697c62893f689fa961282ac86bff9e6b12d718d
Michael Brooks <mikeyabrooks@gmail.com>
2018-02-17 13:13:17 -0800

    adds test for removal of cards when dealt

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -1,11 +1,8 @@
 require './deck'
 
 RSpec.describe Deck do
-  subject(:deck) { Deck.new }
-  let(:deck2) do
-    Deck.new([Card.new(:clubs, 3), Card.new(:diamonds, 4)])
-  end
 
+  let(:deck) { Deck.new }
 
   describe "initialize" do
     it "has 52 cards" do
@@ -15,10 +12,13 @@ RSpec.describe Deck do
     it 'has a default deck of unique cards' do
       expect(deck.cards.uniq).to eq(deck.cards)
     end
-
-    # it "has 13 values"
   end
 
-  # check shuffle compare 1 card after two shuffles
+  describe "deal" do
+    it "removes dealt cards from deck" do
+      deck.deal(7)
+      expect(deck.cards.length).to eq(45)
+    end
+  end
 
 end
