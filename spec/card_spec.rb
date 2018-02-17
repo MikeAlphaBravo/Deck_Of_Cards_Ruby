424d9fda1b366e66952a5175d27a975714310c88
Michael Brooks <mikeyabrooks@gmail.com>
2018-02-17 12:53:05 -0800

    add card_spec initialize tests

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,1 +1,23 @@
+require './card'
 
+RSpec.describe Card do
+  subject(:card) { Card.new('5', :Hearts) }
+  let(:card2) { Card.new('10', :Spades) }
+
+  describe '#initialize' do
+    it "initializes with suit" do
+      expect(card.suit).to eq(:Hearts)
+    end
+
+    it "initializes with value" do
+      expect(card.value).to eq('5')
+    end
+  end
+
+  describe '#==(other_card)' do
+    it "tests whether two cards are the same" do
+      expect(card == card2).to be false
+    end
+  end
+
+end
